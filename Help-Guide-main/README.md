a7ff2c7ddd68e99fc5b63a97b36125d26f43f6f9
Brendan Peltzer <31808559+brendanpeltzer@users.noreply.github.com>
2020-10-27 07:55:53 -0700

    Add files via upload

---
+++ Help-Guide-main/README.md
@@ -0,0 +1,3 @@
+# Help-Guide
+BI &amp; Analytics Help Guide
+Single page of html with collapsing sections.
---
+++ Help-Guide-main/helpGuideScript.js
@@ -0,0 +1,58 @@
+﻿var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++)
+{
+  coll[i].addEventListener("click", function()
+  {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight)
+    {
+      content.style.maxHeight = null;
+    } else
+    {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
+ }
+
+function openContent()
+{
+	//var hash = window.location.hash; //Leaves # character
+	//alert("This is what is selected: " + hash);
+	//var linkedContent = document.getElementById("anchor").name;
+	//var anchors = document.anchors[0].innerHTML;
+	//var selectedContent = document.getElementsByName(linkedContent).elements;
+	//var selectedContent = linkedContent.nextElementSibling;
+
+	if (window.location.hash)
+	{
+		var hash = window.location.hash; //Leaves # character
+		alert("This is what is selected: " + hash);
+		return (hash);
+		hash.style.maxHeight = hash.scrollHeight + "px";
+	}
+	else
+	{
+		return ('');
+	}
+};
+
+/*$(document).ready(function()
+{
+   $(expandByAnchor()).removeClass('mw-collapsed');  
+});
+ 
+var expandByAnchor = function()
+{
+   if(window.location.hash) 
+   {
+      var hash = window.location.hash; //Leaves # character
+      return (hash);      
+   } 
+   else
+   {
+      return ('');
+   }
+};*/
